13315d293f5dcf30c2a23568d2ab18eadf8fff05
Kimee <wujm002@galanz.com>
2019-11-20 14:28:52 +0800

    no message

--- LoginServiceInterface/Classes/LoginServiceInterface.swift
+++ LoginServiceInterface/Classes/LoginServiceInterface.swift
@@ -8,12 +8,12 @@
 @_exported import KLServer
 
 public extension KLServer {
-    @objc public func login(with parameters:  NSDictionary?) -> UIViewController? {
+    @objc public func login(with parameters:  NSDictionary?) -> UIViewController {
         var params = (parameters ?? [:]) as! [AnyHashable : Any]
         params.updateValue("LoginService", forKey: kKLServerParamsKey)
         if let vc = self.performService("LoginService", task: "nativeToLogin", params: params, shouldCacheService: false) as? UIViewController {
             return vc
         }
-        return nil
+        return UIViewController()
     }
 }
