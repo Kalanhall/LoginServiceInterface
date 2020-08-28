9b6aa4659b1ce4a4f6373e3796e26d4d95b7d0df
692015 <wujm002@galanz.com>
2020-08-28 08:42:09 +0800

    no message

--- LoginServiceInterface/Classes/LoginServiceInterface.swift
+++ LoginServiceInterface/Classes/LoginServiceInterface.swift
@@ -5,13 +5,14 @@
 //  Created by Logic on 2019/11/14.
 //
 
-@_exported import KLServer
+@_exported import CTMediator
 
-public extension KLServer {
+public extension CTMediator {
     @objc public func login(with parameters:  NSDictionary?) -> UIViewController {
         var params = (parameters ?? [:]) as! [AnyHashable : Any]
-        params.updateValue("LoginService", forKey: kKLServerParamsKey)
-        if let vc = self.performService("LoginService", task: "nativeToLogin", params: params, shouldCacheService: false) as? UIViewController {
+        params.updateValue("HomeService", forKey: kCTMediatorParamsKeySwiftTargetModuleName)
+        
+        if let vc = self.performTarget("LoginService", action: "nativeToLogin", params: params, shouldCacheTarget: false) as? UIViewController {
             return vc
         }
         return UIViewController()
